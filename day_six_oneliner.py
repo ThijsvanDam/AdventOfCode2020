8226fe3201eec7bf48f73cbb9ca5551908f9ae9c
ThijsvanDam <thijsvandam@msn.com>
2020-12-06 22:27:11 +0100

    even more c

--- day_six_oneliner.py
+++ day_six_oneliner.py
@@ -3,5 +3,5 @@ def get_distinct_letter_count_per_group_sum_oneliner(i):
 
 
 def get_unanimous_letter_count_per_group_sum_oneliner(i):
-    return sum(sum([(1 if x[0].count(j) >= x[1] else 0) for j in set(x[0])]) for x in
+    return sum(sum([(1 if i[0].count(j) >= i[1] else 0) for j in set(i[0])]) for i in
                [(k.replace("\n", ""), k.count("\n") + 1) for k in i.split("\n\n")])

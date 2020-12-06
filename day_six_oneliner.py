cc28411004c079e89687638c4f1375c1b9693487
ThijsvanDam <thijsvandam@msn.com>
2020-12-06 22:20:58 +0100

    input-->i

--- day_six_oneliner.py
+++ day_six_oneliner.py
@@ -1,7 +1,7 @@
-def get_distinct_letter_count_per_group_sum_oneliner(input):
-    return sum(map(lambda j: len("".join(set(j))), [x.replace("\n", "") for x in input.split("\n\n")]))
+def get_distinct_letter_count_per_group_sum_oneliner(i):
+    return sum(map(lambda j: len("".join(set(j))), [x.replace("\n", "") for x in i.split("\n\n")]))
 
 
-def get_unanimous_letter_count_per_group_sum_oneliner(input):
+def get_unanimous_letter_count_per_group_sum_oneliner(i):
     return sum(sum([(1 if x[0].count(j) >= x[1] else 0) for j in set(x[0])]) for x in
-               [(x.replace("\n", ""), x.count("\n") + 1) for x in input.split("\n\n")])
+               [(x.replace("\n", ""), x.count("\n") + 1) for x in i.split("\n\n")])
